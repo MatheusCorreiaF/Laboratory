da77db94272846985ad75e0724353190fab2a24f
Matheus Correia <matheuscf.redfield@gmail.com>
2024-03-27 19:14:26 -0300

    Update ClientFeign.java

--- Feign/consumerFeign/src/main/java/br/com/redfield/consumerFeign/client/ClientFeign.java
+++ Feign/consumerFeign/src/main/java/br/com/redfield/consumerFeign/client/ClientFeign.java
@@ -2,15 +2,15 @@ package br.com.redfield.consumerFeign.client;
 
 import br.com.redfield.consumerFeign.model.Age;
 import br.com.redfield.consumerFeign.model.Client;
-import feign.Body;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "clientFeignClient",
             url = "localhost:8181/v1/client")
 public interface ClientFeign {
 
-    @PostMapping
-    Client getClient(Age age);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    Client getClient(@RequestBody Age age);
 }
